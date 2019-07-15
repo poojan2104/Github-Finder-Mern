69e56cef80b965134138114f138777cb1c41eaa4
poojanpatel-bacancy <46162166+poojanpatel-bacancy@users.noreply.github.com>
2019-07-15 16:27:11 +0530

    16 vedeo completed

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -5,10 +5,18 @@ import axios from "axios";
 import "./App.css";
 
 class App extends Component {
-  componentDidMount() {
-    axios
-      .get("https://api.github.com/users")
-      .then(res => console.log(res.data));
+  state = {
+    users: [],
+    loading: false
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const res = await axios.get(`https://api.github.com/users?client_id=
+    ${process.env.REACT_APP_GITHUB_CLIENT_Id}&client_secret=
+    ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    this.setState({ users: res.data, loading: false });
   }
 
   render() {
@@ -16,7 +24,7 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
